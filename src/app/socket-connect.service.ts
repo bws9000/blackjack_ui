757dcc726e5d0783f9273dc6bf1c4cdeaa3f068f
bws9000 <armorsoft@gmail.com>
2019-10-22 22:21:31 -0400

    env update

--- src/app/socket-connect.service.ts
+++ src/app/socket-connect.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import * as io from 'socket.io-client';
 import { Observable } from 'rxjs';
-import { environment } from './../environments/environment';
+import { environment } from 
 
 @Injectable({
   providedIn: 'root'
