1814955796b9c21833c8f847769c86cf6767ba8d
bws9000 <armorsoft@gmail.com>
2019-12-03 21:28:39 -0500

    update environment

--- env.bat
+++
@@ -1,5 +0,0 @@
-set MONGODB_URI=mongodb://heroku_mlab_bws9000:xX2388x3itH4@ds139775.mlab.com:39775/heroku_67lc8qvd
-set DEV_PASS=bwiley8FGGG
-set SOCKET_URL=https://calm-eyrie-37824.herokuapp.com/blackjack
-set DEV_SOCKET_URL=http://localhost:4444
-set ADMIN_PASS=x45x*fdsGSH9!
